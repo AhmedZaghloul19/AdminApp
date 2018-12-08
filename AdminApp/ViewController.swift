f0fcfe564733a01bee09a260c144cfde688c2175
Ahmed Zaghloul <a.mohamed.zaghloul@gmail.com>
2018-12-08 13:27:33 +0200

    Initial Commit

---
+++ AdminApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdminApp
+//
+//  Created by Ahmed Zaghloul on 12/8/18.
+//  Copyright © 2018 Ahmed Zaghloul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
